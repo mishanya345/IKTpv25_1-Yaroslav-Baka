1c2a3bb034d5f00471aac6d0b52b0b19d3f4caa4
opilane <opilane@HP-CZC2349HTQ>
2025-11-20 11:46:02 +0200

    mäng game kivi paber käärid

---
+++ listid.py
@@ -0,0 +1,108 @@
+﻿#list
+#loome listi
+
+from itertools import count
+
+
+l=[]
+print(f'listi tüüp on {l}')
+while True:
+    print("tee valik:\n1. lisa elemente\n2. lisa elemente pos-le\n3. Eemalda elemente pos järgi\n4. eemade elemen nime järgi\n5. sorteerida elementid\n6. reverseda elemtid \n7. clear kõik \n8.  teise listi elementid lõppu")
+    while True:
+        try:
+            valik=int(input())
+            break
+        except:
+            print('arvnud:1...n')
+    print('töö listiga')
+    if valik==1:
+        while True:
+            try:
+                i=int(input('mitu elementi soovid listi lisada? '))
+                if i>0:
+                    break
+                else:
+                    print('palun sisesta arvnud suurem 0!')
+            except:
+                print('palun sisesta arvnud !')
+               
+        for e in range(i):
+            element=input(f'sisesta {e}. element: ')
+            l.append(element)
+        print(f'sinu list on nüüd: {l}')
+    elif valik==2:
+        while True:
+            try:
+                pos=int(input(f'sisesta positsioon kuhu soovid lisada : '))
+                if 0<=pos<=len(l):
+                    break
+                else:
+                    print(f'palun sisesta arvnud vahemikus 0-{len(l)}')
+            except:
+                print('palun sisesta arvnud!')
+        element=input(f'sisesta element: ')
+        l.insert(pos,element)
+        print(f'sinu list on nüüd: {l}')
+    elif valik==3:
+         while True:
+            try:
+                pos=int(input(f'sisesta positsioon kust soovid eemaldada (0-{len(l)-1}): '))
+                if 0<=pos<=len(l)-1:
+                    break
+                else:
+                    print(f'palun sisesta arvnud vahemikus 0-{len(l)-1}')
+            except:
+                print('palun sisesta arvnud!')
+         eem_element=l.pop(pos)
+         print('eemaldanud element on:', eem_element)
+         print(f'sinu list on nüüd: {l}, eemaldasid elemendi: {eem_element}')
+
+    elif valik==4:
+
+        element=input('sisesta element mida soovid eemaldada: ')
+        mitu=l.count(element)
+        if mitu==0:
+            print('elementi ei leitud listist!')
+            break
+        else:
+            for i in range(mitu):
+                print(f'eemaldan {element}. korda elementi: {l.index(element)}')
+                l.remove(element)
+                
+        print(f'eemaldasid {mitu} korda elementi: {element}')
+        
+
+        #loend.extend()
+        #loend.sort()
+        #loend.reverse()
+        #loend.clear()
+    elif valik==5:
+        l.sort()
+        print(f'sinu list on nüüd: {l}')
+
+
+    elif valik==6:
+        l.reverse()
+        print(f'sinu list on nüüd: {l}')
+
+    elif valik==7:
+        l.clear()
+        print(f'sinu list on nüüd: {l}')
+
+    elif valik==8:
+        teine_list=[]
+        while True:
+            try:
+                i=int(input('mitu elementi soovid teise listi lisada? '))
+                if i>0:
+                    break
+                else:
+                    print('palun sisesta arvnud suurem 0!')
+            except:
+                print('palun sisesta arvnud !')
+               
+        for e in range(i):
+            element=input(f'sisesta {e}. element: ')
+            teine_list.append(element)
+        l.extend(teine_list)
+        print(f'sinu list on nüüd: {l}')
--- töö4.2 kivi paber-käärid .py
+++ töö4.2 kivi paber-käärid .py
@@ -1,31 +1,49 @@
 ﻿# Mäng kivi paber käärid
+import msvcrt
 import random
-valikud = ["kivi", "paber", "käärid"]
-user1 = input('mis on sinu nimi mängija 1? ')
-user2 = input('mis on sinu nimi mängija 2? ')
-print('kivi... käärid... paber... üks... kaks... kolm')
-game = True
-while game:
+
+valikud = ["kivi\r", "paber\r", "käärid\r"]
+while True:
     try:
-        user1 = input("Mängija 1 vali kivi, paber või käärid: ").lower()
-        user2 = input("Mängija 2 vali kivi, paber või käärid: ").lower()
-        if user1 in valikud and user2 in valikud:
-            break
+
+        user1 = input('mis on sinu nimi mängija 1? ')
+        user2 = input('mis on sinu nimi mängija 2? ')
+        break
     except:
-        print("Palun vali kivi, paber või käärid.")
-while game:
+        print("Palun sisesta koreekntne nimi .")
+print('kivi... käärid... paber... üks... kaks... kolm')
+while True:
+        user1 = ''
+        print('Mängija 1 vali kivi, paber või käärid: ', end='')
+        while True:
+            t = msvcrt.getch().decode('utf-8')
+            print(t.replace(t, '*'), end='', flush=True)
+            user1 += t
+            if t =='\r':
+                break
+        print()
+        print(user1)
+        if user1 in valikud:
+            break
+        else:
+
+            print("Palun vali kivi, paber või käärid.")
+while True: 
+     
+        user2 = input("Mängija 2 vali kivi, paber või käärid: ").lower()
+        if user2 in valikud:
+            break
+        else:
+            print("Palun vali kivi, paber või käärid.")
+while True:
     if user1 == user2:
         print("mitte keegi!")
+        break
     elif (user1 == "kivi" and user2 == "käärid") or (user1 == "paber" and user2 == "kivi") or (user1 == "käärid" and user2 == "paber"):
         print("Mängija 1 võidab!")
-        game = False
+        break
     elif (user2 == "kivi" and user1 == "käärid") or (user2 == "paber" and user1 == "kivi") or (user2 == "käärid" and user1 == "paber"):
         print("Mängija 2 võidab!")
-        game = False
-küsimus = input("Kas soovite uuesti mängida? (jah/ei): ").lower()
-if küsimus != "jah":
-    game = False
-    print("Aitäh mängimast!")
-
+        break
 
 
