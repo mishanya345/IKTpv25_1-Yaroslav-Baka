12583c847720d8e9b1cc97065f6608ddc326c680
opilane <opilane@HP-CZC2349HTQ>
2025-12-04 10:16:09 +0200

    save

---
+++ Tavalinefail.py
@@ -0,0 +1,13 @@
+﻿from modulifail import*
+
+print("Lahendame/testime 5 arvutehet")
+for i in range(5):
+
+    arv1 = float_kontroll(input("sisesta esimene arv: "))
+    arv2 = float_kontroll(input("sisesta esimene arv: "))
+
+t=input("sisesta tehe(+,-,*,/):")
+tulemus=arithmetic(arv1,arv2,t)
+print(arv1,arv2,t)
+print(f"{arv1} {t} {arv2} = {tulemus}")
+
---
+++ modulifail.py
@@ -0,0 +1,33 @@
+﻿#1 töö 5.1
+# ül. 1
+
+def arithmetic(a: float, b: float,tehe: str)->any:
+    """Lihtne kalkulaator:
+    +- liitmine
+    -- lahutamine
+    */ korrutamine
+    /-jagamine
+    kul juhul tagastab "tundmatu tehe"
+    :param float a: esimene arv
+    :param float b: teine arv 
+    :param str tehe: tehe, mis tuleb teha
+    :return/rtype: tehte tulemus float või str"""
+    if tehe in ["+","-","*","/"]:
+        if tehe == "/" and b==0:
+            vastus= "nulliga jagamine pole lubatud"
+        else:
+            vastus=eval(f"{a}{tehe}{b}")
+    else:
+        vastus="Tundmatu tehe"
+    return vastus
+
+def float_kontroll(sisend:str)->float:
+    """Kontrollib, kas sisestatud andmed on teisendavad float arvuks 
+    : param str sisend: kasutaja sisestatud andmed
+    : return/rtype: teisendatud float arv """
+    while True:
+        try:
+            arv1 = float(input("sisesta esimene arv: "))
+            break
+        except: 
+            print("palun sisesta arv!")
