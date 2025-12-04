23ccadb63c0b3b3af0d6693f602febc1613962d3
opilane <opilane@HP-CZC2349HTQ>
2025-12-04 11:45:01 +0200

    save2

--- Tavalinefail.py
+++ Tavalinefail.py
@@ -1,13 +1,43 @@
-﻿from modulifail import*
+﻿#1
+from modulifail import*
 
-print("Lahendame/testime 5 arvutehet")
+
+'''print("Lahendame/testime 5 arvutehet")
 for i in range(5):
-
     arv1 = float_kontroll(input("sisesta esimene arv: "))
-    arv2 = float_kontroll(input("sisesta esimene arv: "))
+    arv2 = float_kontroll(input("sisesta teine arv: "))
+    t = input("sisesta tehe(+,-,*,/):")
 
-t=input("sisesta tehe(+,-,*,/):")
-tulemus=arithmetic(arv1,arv2,t)
-print(arv1,arv2,t)
-print(f"{arv1} {t} {arv2} = {tulemus}")
+    tulemus = arithmetic(arv1, arv2, t)
+    print(arv1, arv2, t)
+    print(f"{arv1} {t} {arv2} = {tulemus}")'''
 
+
+#2
+
+'''for i in range(3):
+    aasta=int_kontroll(input("Sisesta aasta: "))
+    if is_year_leap(aasta):
+        print(f"{aasta} on liigaasta")
+    else:
+        print(f"{aasta} on tavaline aasta")'''
+
+
+#3
+'''
+külje_pikkus=float_kontroll(input("Sisesta ruudu külje pikkus: "))
+pindala, ümbermõõt, diagonaal=square(külje_pikkus)
+print(f"Ruudu pindala on {pindala}, ümbermõõt on {ümbermõõt}, diagonaal on {diagonaal}")'''
+
+#4
+'''
+kuu=int_kontroll(input("Sisesta kuu number (1-12): "))
+kuu=season(kuu)
+print(f"on aastaaja {kuu}")'''
+
+#5
+
+arv = int_kontroll(input("Sisesta positiivne täisarv: "))
+aasta = int_kontroll(input("Sisesta aasta: "))
+arv1 = pangahoius(arv)
+print(f"Sisestasid arvu pärast kontrolli: {arv1}")
--- modulifail.py
+++ modulifail.py
@@ -27,7 +27,64 @@ def float_kontroll(sisend:str)->float:
     : return/rtype: teisendatud float arv """
     while True:
         try:
-            arv1 = float(input("sisesta esimene arv: "))
-            break
-        except: 
-            print("palun sisesta arv!")
+            arv = float(sisend)
+            return arv
+        except:
+            sisend = input("palun sisesta ainult arv!")
+
+
+def int_kontroll(sisend:str)->int:
+    """Kontrollib, kas sisestatud andmed on teisendavad int arvuks 
+    : param str sisend: kasutaja sisestatud andmed
+    : return/rtype: teisendatud int arv """
+    while True:
+        try:
+            return int(sisend)
+        except:
+            print("palun sisesta täisarv:  ")
+        break
+# ül. 2
+def is_year_leap(aasta: int) -> bool:
+    """Kontrollib, kas aasta on liigaasta
+    :param int aasta: aasta arvuna
+    :return/rtype: True kui liigaasta, False kui tavaline aasta"""
+
+    if (aasta % 4 == 0 and aasta % 100 != 0):
+        return True
+    else:
+        return False
+    
+#ül. 3
+def square(külg: float) -> any:
+    """Arvutab ruudu pindala, ümbermõõdu ja diagonaali
+    :param float külg: ruudu külje pikkus
+    :return/rtype: tuple (pindala, ümbermõõt, diagonaal)"""
+    pindala = külg ** 2
+    ümbermõõt = 4 * külg
+    diagonaal = round((külg * 2 ** 0.5), 2)
+    return (pindala, ümbermõõt, diagonaal)
+
+#ül. 4
+def season(kuu: int) -> str:
+    """Määrab kuu põhjal aastaaja
+    :param int kuu: kuu arvuna (1-12)
+    :return/rtype: str aastaaja nimetus"""
+    if kuu in [12, 1, 2]:
+        return "talv"
+    elif kuu in [3, 4, 5]:
+        return "kevad"
+    elif kuu in [6, 7, 8]:
+        return "suvi"
+    elif kuu in [9, 10, 11]:
+        return "sügis"
+    else:
+        return "vigane kuu"
+    
+def pangahoius(a:float, years:int) -> float:
+    """Arvutab pangahoiuse lõppsumma koos intressidega
+    :param float a: algne hoiusesumma
+    :param int years: aastate arv
+    :return/rtype: float lõppsumma"""
+    for i in range(years):
+        a = a * 1.1
+    return round(a, 2)
