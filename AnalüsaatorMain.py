43717f85229026f06546cdd55c4fe48b59ec1431
BakaYaroslav <jaroslavbaka25@gmail.com>
2026-01-11 23:05:09 +0200

    funktioonid on valmis +-

--- AnalüsaatorMain.py
+++ AnalüsaatorMain.py
@@ -1,10 +1,34 @@
 import analüsaatorModule
+import os
+print(os.getcwd())
+failid = os.listdir()
+laiendid = set()
+for f in failid:
+    if '.' in f:
+        laiendid.add(f.split('.')[-1])
+        
+print(f"Leitud faililaiendid: {laiendid}")
 
 while True:
+    valik = input("Vali tegevus:\n1. Leia projektifailid\n2. Analüüsi faili sisu\n3. Loo raporti kataloog\n4. Leia failid algustähega\n5. Välju\nSisesta valik (1-5): ")
+    if valik not in ['1', '2', '3', '4', '5']:
+        print("Vigane valik, proovi uuesti.")
     try:
-        faililaend = analüsaatorModule.leia_projektifailid()
-        analuus = analüsaatorModule.analuusi_faili_sisu()
-        raport = analüsaatorModule.loo_raporti_kataloog()
-        break
+        if valik == '1':
+            faililaend = analüsaatorModule.leia_projektifailid()2
+
+        elif valik == '2':
+            analuus = analüsaatorModule.analuusi_faili_sisu()
+
+        elif valik == '3':
+            raport = analüsaatorModule.loo_raporti_kataloog()
+
+        elif valik == '4':
+            algustahed = analüsaatorModule.leia_failid_algustahega()
+
+        elif valik == '5':
+            print("Väljutamine...")
+            break
+    
     except:
         print("Ilmnes viga, proovi uuesti.")
--- analüsaatorModule.py
+++ analüsaatorModule.py
@@ -15,27 +15,41 @@ def analuusi_faili_sisu():
         else:
             fail = open(failitee, 'r', encoding='utf-8')
             sisu = fail.read()
+            sõna = input("Sisesta sõna, mida otsida: ")
+            sõna_count = sisu.lower().count(sõna.lower())
+        
             fail.close()
             print(f"Sisu: {sisu}")
+            print(f"Sõna '{sõna}' esinemiste arv: {sõna_count}")
             break
     return sisu
 
 
 def loo_raporti_kataloog():
     while True:
-        failitee = input("Sisesta : ")
-        if not os.path.isfile(failitee):
-            ask = input("Faili ei leitud, proovi uuesti. Kas te tehete lisada faili? (jah/ei)")
+        failitee = input("Sisesta raporti kataloogi tee: ")
+        if not os.path.isdir(failitee):
+            ask = input("Kataloogi ei leitud, proovi uuesti. Kas soovid lisada kataloogi? (jah/ei)")
             if ask.lower() == 'jah':
-                os.mkdir().failitee
-                print("Faili on lisatud.")
+                os.mkdir(failitee)
+                print("Kataloog on lisatud.")
             else:
-                continue
+                print("Proovi uuesti.")
         else:
-            print("Fail leitud. ")
+            print("Kataloog leitud. ")
             break
     return failitee
 
+def leia_failid_algustahega():
+    algustaht = input("Sisesta algustäht: ")
+    while True:
+        if not algustaht.isalpha() or len(algustaht) != 1:
+            print("Palun sisesta üks täht.")
+        else:
+            break
+    failid = glob.glob(f'{algustaht}*')
+    print(f"Leitud failid: {failid}")
+    return failid
 
 
 
