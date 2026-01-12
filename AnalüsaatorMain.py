0ad0f8573543451d46b33e8f4d77709b5a6ef400
BakaYaroslav <jaroslavbaka25@gmail.com>
2026-01-12 17:54:36 +0200

    Kood töötab normaalselt

--- AnalüsaatorMain.py
+++ AnalüsaatorMain.py
@@ -6,29 +6,34 @@ laiendid = set()
 for f in failid:
     if '.' in f:
         laiendid.add(f.split('.')[-1])
-        
-print(f"Leitud faililaiendid: {laiendid}")
-
+stat = []        
 while True:
     valik = input("Vali tegevus:\n1. Leia projektifailid\n2. Analüüsi faili sisu\n3. Loo raporti kataloog\n4. Leia failid algustähega\n5. Välju\nSisesta valik (1-5): ")
     if valik not in ['1', '2', '3', '4', '5']:
         print("Vigane valik, proovi uuesti.")
     try:
         if valik == '1':
-            faililaend = analüsaatorModule.leia_projektifailid()2
-
+            faililaend = analüsaatorModule.leia_projektifailid()
+            stat.append(faililaend)
         elif valik == '2':
             analuus = analüsaatorModule.analuusi_faili_sisu()
-
+            stat.append(analuus)
         elif valik == '3':
             raport = analüsaatorModule.loo_raporti_kataloog()
-
+            stat.append(raport)
         elif valik == '4':
             algustahed = analüsaatorModule.leia_failid_algustahega()
-
+            stat.append(algustahed)
         elif valik == '5':
             print("Väljutamine...")
             break
     
     except:
         print("Ilmnes viga, proovi uuesti.")
+
+if os.path.isfile('statistika.txt'):
+    os.remove("statistika.txt")
+with open("statistika.txt", 'w', encoding='utf-8') as f:
+    for rida in stat:
+        f.write(str(rida) + "\n")
+print("Statistika on salvestatud faili 'statistika.txt'.")
--- analüsaatorModule.py
+++ analüsaatorModule.py
@@ -5,7 +5,7 @@ def leia_projektifailid():
     fail = input("sisesta faililaiendi ilma punktita: ")
     failid = glob.glob(f'*.{fail}')
     print(f"Leitud failid: {failid}")
-    return failid
+    return f"'.'+{fail} failid, {failid}"
 
 def analuusi_faili_sisu():
     while True:
@@ -17,39 +17,68 @@ def analuusi_faili_sisu():
             sisu = fail.read()
             sõna = input("Sisesta sõna, mida otsida: ")
             sõna_count = sisu.lower().count(sõna.lower())
+            length = len(sisu)
         
             fail.close()
             print(f"Sisu: {sisu}")
             print(f"Sõna '{sõna}' esinemiste arv: {sõna_count}")
+            print(f"Faili pikkus: {length} tähemärki")
             break
-    return sisu
+    return f"{sõna_count}\nFaili pikkus: {length}\nSISU FAILIST:\n {sisu}\n"
 
 
 def loo_raporti_kataloog():
     while True:
         failitee = input("Sisesta raporti kataloogi tee: ")
-        if not os.path.isdir(failitee):
-            ask = input("Kataloogi ei leitud, proovi uuesti. Kas soovid lisada kataloogi? (jah/ei)")
-            if ask.lower() == 'jah':
+        ask = input("Kas soovid lisada või kustutada või otsida kataloogi? (l/k/o)").lower()
+        
+        if ask == 'l':
+            if not os.path.isdir(failitee):
                 os.mkdir(failitee)
-                print("Kataloog on lisatud.")
+                return f"Kataloog {failitee} on loodud"
             else:
-                print("Proovi uuesti.")
-        else:
-            print("Kataloog leitud. ")
-            break
-    return failitee
+                print("Kataloog on juba olemas.")
+                break
+
+                
+        elif ask == 'k':
+            if not os.path.isdir(failitee):
+                print("Kataloogi ei leitud. Palun proovi uuesti.")  
+                os.rmdir(failitee)
+                return f"Kataloog {failitee} on kustutatud"
+            else:
+                try:
+                    ask2 = input("Kataloogi ei leitud, kas soovid just kustutada? (jah/ei)")
+                    if ask2.lower() == 'jah':
+                        os.rmdir(failitee)
+                        return f"Kataloog {failitee} on kustutatud"
+                    elif ask2.lower() == 'ei':
+                        print("Kataloogi kustutamine katkestatud.")
+                        break
+                except:
+                    print("palun kirjuta jah või ei")
+                    break
+                
+        elif ask == 'o':
+            if not os.path.isdir(failitee):
+                print("Kataloogi ei leitud.")
+            else:
+                print(f"Kataloog {failitee} on leitud.")
+                break
+      
+    return f"{failitee} on leitud"
 
 def leia_failid_algustahega():
-    algustaht = input("Sisesta algustäht: ")
+
     while True:
+        algustaht = input("Sisesta algustäht: ")
         if not algustaht.isalpha() or len(algustaht) != 1:
             print("Palun sisesta üks täht.")
         else:
             break
     failid = glob.glob(f'{algustaht}*')
     print(f"Leitud failid: {failid}")
-    return failid
+    return f"algustäht failid {algustaht}:\nFailid: {failid}\n Kui palju: {len(failid)}"
 
 
 
